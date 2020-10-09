acd26b8806f0e7b71514fb8b06b2ecd10b343df0
Tcaprioli <Capriolityler@gmail.com>
2020-10-09 09:08:00 -0400

    api set up and connected to db

---
+++ src/models/user.ts
@@ -0,0 +1,11 @@
+import mongoose,{ Schema,Document } from 'mongoose'
+
+export interface IUser extends Document{
+  name: string
+}
+
+const UserSchema: Schema = new Schema({
+  name: {type: String}
+})
+
+export const User = mongoose.model<IUser>("User", UserSchema.obj, "Users")
---
+++ src/typeDefs.ts
@@ -0,0 +1,19 @@
+import { gql } from 'apollo-server-express'
+
+export const typeDefs = gql`
+
+type Query {
+  helloWorld: String
+  users: [User!]!
+}
+
+type User {
+  id: ID!
+  name: String!
+}
+
+type Mutation {
+  createUser(name: String!): User!
+}
+
+`
